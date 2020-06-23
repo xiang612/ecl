1f376770ce2ca7c439055cef40edf8470712a8b5
Daniel Agar <daniel@agar.ca>
2020-06-22 22:24:45 -0400

    geo_lookup: upgrade table size (2 bytes per element) and add testing

--- geo_lookup/geo_mag_declination.h
+++ geo_lookup/geo_mag_declination.h
@@ -41,11 +41,14 @@
 
 #pragma once
 
-// Return magnetic declination in degrees
-float get_mag_declination(float lat, float lon);
+// Return magnetic declination in degrees or radians
+float get_mag_declination_degrees(float lat, float lon);
+float get_mag_declination_radians(float lat, float lon);
 
-// Return magnetic field inclination in degrees
-float get_mag_inclination(float lat, float lon);
+// Return magnetic field inclination in degrees or radians
+float get_mag_inclination_degrees(float lat, float lon);
+float get_mag_inclination_radians(float lat, float lon);
 
-// return magnetic field strength in centi-Tesla
-float get_mag_strength(float lat, float lon);
+// return magnetic field strength in Gauss or Tesla
+float get_mag_strength_gauss(float lat, float lon);
+float get_mag_strength_tesla(float lat, float lon);
